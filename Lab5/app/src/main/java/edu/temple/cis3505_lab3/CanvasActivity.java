b82ef1979b207e4d412811d661b94d081e67231c
hdang-Git <hi4sure@gmail.com>
2016-10-20 09:45:06 -0400

    Lab 5 added res file for language

---
+++ Lab5/app/src/main/java/edu/temple/cis3505_lab3/CanvasActivity.java
@@ -0,0 +1,25 @@
+package edu.temple.cis3505_lab3;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+public class CanvasActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_canvas);
+
+        RelativeLayout view = (RelativeLayout) findViewById(R.id.activity_canvas);
+
+        //get the intent used to launch this instance of activity
+        Intent received = getIntent();
+        String dataReceived = received.getStringExtra("colorStuff");
+        view.setBackgroundColor(Color.parseColor(dataReceived));
+    }
+}
---
+++ Lab5/app/src/main/java/edu/temple/cis3505_lab3/CustomAdapter.java
@@ -0,0 +1,40 @@
+package edu.temple.cis3505_lab3;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Hai Dang on 9/22/2016.
+ */
+
+public class CustomAdapter extends ArrayAdapter {
+
+    String colors[] = null;
+    static final String[] actualColors = {"WHITE","RED", "BLUE", "GREEN", "YELLOW"};
+
+    public CustomAdapter(Context context, int resource, Object[] objects) {
+        super(context, resource, objects);
+        this.colors = (String[]) objects;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View v = super.getView(position, convertView, parent);
+        v.setBackgroundColor(Color.parseColor(actualColors[position]));
+        return v;
+    }
+
+    @Override
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+        View v = super.getView(position, convertView, parent);
+        v.setBackgroundColor(Color.parseColor(actualColors[position]));
+        return v;
+    }
+}
