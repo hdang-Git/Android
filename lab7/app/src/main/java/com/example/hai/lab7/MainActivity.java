7b17a095dd38292e3c0a6bd1ea7b88685e01a4c3
hdang-Git <hdang0010@gmail.com>
2016-11-07 01:15:51 -0500

    Initial commit: Added actionbar with icons"

---
+++ lab7/app/src/main/java/com/example/hai/lab7/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.hai.lab7;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.icons, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_back:
+                // User chose the "back" item...
+                return true;
+
+            case R.id.action_forward:
+                // User chose the "forward" action
+                return true;
+            case R.id.action_refresh:
+                //User chose the "refresh" action
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
