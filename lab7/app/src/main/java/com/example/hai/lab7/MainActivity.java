506ca6a1d10e0901ec364c5626b8087d88e0f504
hdang-Git <hdang0010@gmail.com>
2016-11-08 05:40:30 -0500

    Added editText view into toolbar. Added webview fragment. Blocked title in mainActivity using ActionBar

--- lab7/app/src/main/java/com/example/hai/lab7/MainActivity.java
+++ lab7/app/src/main/java/com/example/hai/lab7/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.hai.lab7;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +16,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+
+        EditText textField = (EditText) findViewById(R.id.editText);
+
+        //Disable title
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayShowTitleEnabled(false);
     }
 
     @Override
