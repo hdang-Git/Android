acba148b700d83a30573d0901c58b24242b09531
hdang-Git <hi4sure@gmail.com>
2016-11-08 19:20:14 -0500

    Changed layout. Added web fragment. Implemented editText to read on click event on refresh icon.

---
+++ Lab7/app/src/main/java/com/example/hai/lab7/webFrag.java
@@ -0,0 +1,40 @@
+package com.example.hai.lab7;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class webFrag extends Fragment {
+
+
+    public webFrag() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_web, container, false);
+    }
+
+}
