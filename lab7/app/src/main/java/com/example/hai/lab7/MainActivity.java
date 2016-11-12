4d7344a25bf6158c84518dc1c386a11a0fdc169b
hdang-Git <hi4sure@gmail.com>
2016-11-12 00:21:37 -0500

    Modified mainActivity and webFrag with impl

--- lab7/app/src/main/java/com/example/hai/lab7/MainActivity.java
+++ lab7/app/src/main/java/com/example/hai/lab7/MainActivity.java
@@ -8,7 +8,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 
+import java.util.logging.Logger;
+
+import static com.example.hai.lab7.R.id.frag1;
+
 public class MainActivity extends AppCompatActivity {
+
+    Logger log = Logger.getAnonymousLogger();
+    EditText textField;
+    String input;
+    webFrag receiver = new webFrag();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +27,17 @@ public class MainActivity extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
-        EditText textField = (EditText) findViewById(R.id.editText);
+        //textField = (EditText) findViewById(R.id.editText);
 
         //Disable title
         ActionBar ab = getSupportActionBar();
         ab.setDisplayShowTitleEnabled(false);
+
+        getFragmentManager()
+                .beginTransaction()
+                .add(frag1, receiver)
+                .commit();
+
     }
 
     @Override
@@ -33,15 +49,38 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()) {
+            case R.id.action_refresh:
+                //User pressed the "refresh" button to refresh the page
+                log.info("Refresh Pressed.");
+                textField = (EditText) findViewById(R.id.editText);
+                input = String.valueOf(textField.getText().toString());
+                log.info("You entered: " + input);
+                receiver.changeURL(input);
+                return true;
+
+            case R.id.action_prev:
+                //User chose the "previous" action
+                log.info("Prev Pressed.");
+                return true;
+
+            case R.id.action_next:
+                //User chose the "next" action
+                log.info("Next Pressed.");
+                return true;
+
+            case R.id.action_new:
+                //User chose the "new" action to get a new window
+                log.info("New Pressed.");
+                return true;
+
             case R.id.action_back:
                 // User chose the "back" item...
+                log.info("Back Pressed.");
                 return true;
 
             case R.id.action_forward:
                 // User chose the "forward" action
-                return true;
-            case R.id.action_refresh:
-                //User chose the "refresh" action
+                log.info("Forward Pressed.");
                 return true;
 
             default:
