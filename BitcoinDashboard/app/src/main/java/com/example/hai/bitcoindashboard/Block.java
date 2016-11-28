ea83bdf2def290e003ce4873c2fa43368628915e
hdang-Git <hdang0010@gmail.com>
2016-11-28 14:22:30 -0500

    Added block activity

---
+++ BitcoinDashboard/app/src/main/java/com/example/hai/bitcoindashboard/Block.java
@@ -0,0 +1,13 @@
+package com.example.hai.bitcoindashboard;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Block extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_block);
+    }
+}
