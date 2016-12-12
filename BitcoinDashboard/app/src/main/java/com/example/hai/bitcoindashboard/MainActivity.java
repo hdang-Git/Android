2cccad49b12974e2bf30476cf4dd914d97e436df
hdang-Git <hdang0010@gmail.com>
2016-12-12 07:00:29 -0500

    Added landscape orientation for graph. Fixed a bug that retained price fragment over the other fragments in landscape mode by using savedinstancestate.

--- BitcoinDashboard/app/src/main/java/com/example/hai/bitcoindashboard/MainActivity.java
+++ BitcoinDashboard/app/src/main/java/com/example/hai/bitcoindashboard/MainActivity.java
@@ -40,8 +40,10 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-        startUp();
 
+        if(savedInstanceState == null) {
+            startUp();
+        }
         ab = getSupportActionBar();
     }
 
